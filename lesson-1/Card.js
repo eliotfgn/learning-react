8de1184a36bb0e6ea37fca10cde64c41b8fa5078
Arsene Awounou <developer.awounou@gmail.com>
2023-01-04 17:17:21 +0100

    move functional component to class component

--- lesson-1/Card.js
+++ lesson-1/Card.js
@@ -1,18 +1,24 @@
-function Card(props) {
-  return (
-    <div className="col s2">
-      <div className="card hoverable small">
-        <div className="card-image">
-          <img src="https://loremflickr.com/640/360" />
-        </div>
-        <div className="card-content">
-          <p>{props.data.course}</p>
-          <p>{props.data.instructor}</p>
-        </div>
-        <div className="card-action">
-          <a href="#">$9.99</a>
+class Card extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="col s2">
+        <div className="card hoverable small">
+          <div className="card-image">
+            <img src="https://loremflickr.com/640/360" />
+          </div>
+          <div className="card-content">
+            <p>{this.props.data.course}</p>
+            <p>{this.props.data.instructor}</p>
+          </div>
+          <div className="card-action">
+            <a href="#">$9.99</a>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
